72c3363232601994bf46d02d08ca0108e9edd7b3
Amiru Noorpi <Amiru.Noorpi@ecenta.com>
2017-04-01 20:07:26 +0800

    First Commit

---
+++ src/com/personal/project/HelloController.java
@@ -0,0 +1,17 @@
+package com.personal.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
